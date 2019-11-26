0043f6cc4b8022733391ef374d1368e104747443
Lucas Melul <lucas.melul@almundo.com>
2019-11-26 03:16:13 -0300

    agrego bot telegram

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const io = require('socket.io');
 const rpiDhtSensor = require('rpi-dht-sensor');
+const TelegramBot = require('node-telegram-bot-api');
+const tokenTelegram = '909712718:AAHV4yTi65jcZHASJEk14NRl-xFm9a6Xf8E';
 
 //  Express Setting
 const app = express();
@@ -57,3 +59,32 @@ function readSensor(socket) {
     humid
   }
 }
+
+
+
+// replace the value below with the Telegram token you receive from @BotFather
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(tokenTelegram, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
